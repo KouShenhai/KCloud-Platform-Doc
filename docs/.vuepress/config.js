3ee18f595fa1bbd2c450b262b038020ccf1ddae8
koushenhai <2413176044@qq.com>
2023-06-10 18:37:33 +0800

    初始化

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -78,7 +78,7 @@ module.exports = {
     },
     footer: { // 页脚信息
       createYear: 2022, // 博客创建年份
-      copyrightInfo: 'laokou | MIT License', // 博客版权信息，支持a标签
+      copyrightInfo: 'laokou | Apache 2.0 License', // 博客版权信息，支持a标签
     },
     htmlModules,
   },
